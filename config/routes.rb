b191168ab76968db42c8e6fe5d0a798be4ce0705
marcwright <marc.wright@gmail.com>
2013-12-26 01:47:35 -0600

    changed root to authentications#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ShopShine::Application.routes.draw do
 
+root to: 'authentications#new'
+
   namespace :api do
     resources :users
   end
@@ -39,7 +41,7 @@ ShopShine::Application.routes.draw do
 
   resources :categories
 
-  root to: 'users#new'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
